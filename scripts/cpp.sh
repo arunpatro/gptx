7d859e577560ca69cd0cea22b8cc0683bf00eb8e
Arun Patro <arun.kumar.patro@gmail.com>
2023-11-29 00:29:03 -0500

    fix

--- scripts/cpp.sh
+++ scripts/cpp.sh
@@ -7,7 +7,7 @@ values=(1 2 4 8 16 32)
 MODEL_PATH="../python/model_weights.json"
 
 # Path to the executable (adjust as needed)
-EXECUTABLE="./target/release/gptx"
+EXECUTABLE="./build/gptx"
 
 # Check if the executable exists
 if [ ! -f "$EXECUTABLE" ]; then
@@ -18,8 +18,8 @@ fi
 # Iterate over the values and run the command
 for t in "${values[@]}"; do
     echo "Running with t=$t"
-    $EXECUTABLE -m $MODEL_PATH -t $t > "rust_$t.txt"
-    echo "Output saved to rust_$t.txt"
+    $EXECUTABLE -m $MODEL_PATH -t $t > "cpp_$t.txt"
+    echo "Output saved to cpp_$t.txt"
 done
 
 echo "All runs completed."
