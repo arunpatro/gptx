8ec51ea02281f193a5e938bf1d9ddee3288e5e49
Arun Patro <arun.kumar.patro@gmail.com>
2023-11-29 00:15:30 -0500

    add

---
+++ scripts/rust.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the values for 't' to iterate over
+values=(1 2 4 8 16 32)
+
+# Path to the model weights (adjust as needed)
+MODEL_PATH="../python/model_weights.json"
+
+# Path to the executable (adjust as needed)
+EXECUTABLE="./target/release/gptx"
+
+# Check if the executable exists
+if [ ! -f "$EXECUTABLE" ]; then
+    echo "Executable not found: $EXECUTABLE"
+    exit 1
+fi
+
+# Iterate over the values and run the command
+for t in "${values[@]}"; do
+    echo "Running with t=$t"
+    $EXECUTABLE -m $MODEL_PATH -t $t > "rust_$t"
+    echo "Output saved to rust_$t"
+done
+
+echo "All runs completed."
